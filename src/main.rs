511466af5fe02bc5585a52fc5fffec3c8140f834
Nicolas Galipot <nicolas.galipot@gmail.com>
2024-08-07 20:43:55 +0200

    add the ability to extract REZ hierarchy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core::str;
-use std::{borrow::Cow, fs::read, path::{Path, PathBuf}};
+use std::{borrow::Cow, env, fs::{create_dir, read, File}, io::Write, path::{Path, PathBuf}};
 
 #[derive(Debug)]
 struct RezHeader<'a> {
@@ -13,80 +13,126 @@ struct RezHeader<'a> {
 }
 
 enum RezEntryContentKind<'a> {
-    Directory,
+    Directory {
+        children: RezDirectoryIterator<'a>,
+    },
     File {
         id: u32,
         content: &'a [u8],
     }
 }
 
-struct RezEntryContent<'a> {
+struct RezEntry<'a> {
     path: PathBuf,
     datetime: u32,
     kind: Option<RezEntryContentKind<'a>>,
 }
 
-struct RezEntry<'a> {
-    content: RezEntryContent<'a>,
+struct RezDirectoryIterator<'a> {
+    input: &'a [u8],
+    path: PathBuf,
+    offset: usize,
+    end: usize,
 }
 
-fn parse_entry<'a>(input: &'a [u8], offset: usize, end: usize, path: &Path, cb: fn (entry: RezEntry)) {
-    if offset >= input.len() {
-        return;
-    }
-    let is_directory = u32::from_le_bytes(input[offset..offset+4].try_into().unwrap()) == 1;
-    let entry_offset = u32::from_le_bytes(input[offset+4..offset+8].try_into().unwrap()) as usize;
-    let entry_size = u32::from_le_bytes(input[offset+8..offset+12].try_into().unwrap()) as usize;
-    if entry_size == 0 {
-        return;
-    }
-    let datetime = u32::from_le_bytes(input[offset+12..offset+16].try_into().unwrap());
-    if is_directory {
-        let mut name_end = offset+16;
-        while input[name_end] != b'\0' && name_end < end  {
-            name_end += 1;
+impl<'a> Iterator for RezDirectoryIterator<'a> {
+    type Item = RezEntry<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.offset >= self.input.len() {
+            return None;
         }
-        let name = String::from_utf8_lossy(&input[offset+16..name_end]);
-        cb(RezEntry {
-            content: RezEntryContent { 
-                path: path.join(&*name), 
-                datetime, 
-                kind: Some(RezEntryContentKind::Directory),
+        let is_directory = u32::from_le_bytes(self.input[self.offset..self.offset+4].try_into().unwrap()) == 1;
+        let entry_offset = u32::from_le_bytes(self.input[self.offset+4..self.offset+8].try_into().unwrap()) as usize;
+        let entry_size = u32::from_le_bytes(self.input[self.offset+8..self.offset+12].try_into().unwrap()) as usize;
+        if entry_size == 0 {
+            return None;
+        }
+        let datetime = u32::from_le_bytes(self.input[self.offset+12..self.offset+16].try_into().unwrap());
+        if is_directory {
+            let mut name_end = self.offset + 16;
+            while self.input[name_end] != b'\0' && name_end < self.end  {
+                name_end += 1;
             }
-        });
-        if entry_size > 0 {
-            parse_entry(input, entry_offset, entry_offset + entry_size, &path.join(&*name), cb);
-        }
-        parse_entry(input, name_end+1, name_end+1+entry_size, path, cb);
-    } else {
-        let file_id = u32::from_le_bytes(input[offset+16..offset+20].try_into().unwrap());
-        let mut extension_end = offset+20;
-        while input[extension_end] != b'\0' && extension_end < end {
-            extension_end += 1;
-        }
-        let reversed_extension = String::from_utf8_lossy(&input[offset+20..extension_end]);
-        let mut name_end = extension_end + 5;
-        while input[name_end] != b'\0' && name_end < end  {
-            name_end += 1;
-        }
-        let mut name = str::from_utf8(&input[extension_end+5..name_end]).expect("string to contain only ascii").to_string();
-        name.push('.');
-        name.extend(reversed_extension.chars().rev());
-        let content = &input[entry_offset..entry_offset+entry_size];
-        cb(RezEntry {
-            content: RezEntryContent { 
-                path: path.join(&*name), 
+            let name = String::from_utf8_lossy(&self.input[self.offset + 16..name_end]);
+            self.offset = name_end + 1;
+            self.end = name_end + 1 + entry_size;
+            let path = self.path.join(&*name);
+            Some(RezEntry {
+                path: path.clone(), 
+                datetime, 
+                kind: Some(RezEntryContentKind::Directory {
+                    children: RezDirectoryIterator {
+                        input: &self.input,
+                        path,
+                        offset: entry_offset,
+                        end: entry_offset + entry_size,
+                    },
+                }),
+            })
+        } else {
+            let file_id = u32::from_le_bytes(self.input[self.offset + 16..self.offset + 20].try_into().unwrap());
+            let mut extension_end = self.offset + 20;
+            while self.input[extension_end] != b'\0' && extension_end < self.end {
+                extension_end += 1;
+            }
+            let reversed_extension = String::from_utf8_lossy(&self.input[self.offset + 20..extension_end]);
+            let mut name_end = extension_end + 5;
+            while self.input[name_end] != b'\0' && name_end < self.end  {
+                name_end += 1;
+            }
+            let mut name = str::from_utf8(&self.input[extension_end + 5..name_end]).expect("string to contain only ascii").to_string();
+            name.push('.');
+            name.extend(reversed_extension.chars().rev());
+            let content = &self.input[entry_offset..entry_offset + entry_size];
+            self.offset = name_end + 2;
+            self.end = name_end + 2 + entry_size;
+            Some(RezEntry {
+                path: self.path.join(&*name), 
                 datetime, 
                 kind: Some(RezEntryContentKind::File { id: file_id, content }),
-            }
-        });
-        parse_entry(input, name_end + 2, name_end + 2 + entry_size, path, cb); 
+            })
+        }
     }
-    
+}
+
+fn display_rez_hierarchy(dir: &mut RezDirectoryIterator) {
+    for entry in dir {
+        match entry.kind {
+            Some(RezEntryContentKind::Directory { mut children }) => {
+                println!(">> dir: {}", entry.path.to_str().expect("path to be valid string"));
+                display_rez_hierarchy(&mut children);
+            }
+            Some(RezEntryContentKind::File { id, content }) => {
+                println!("- file: {}", entry.path.to_str().expect("path to be valid string"));
+            }
+            None => {}
+        }   
+    }
+}
+
+fn extract_rez_hierarchy(dir: &mut RezDirectoryIterator, output_dir: &Path) {
+    for entry in dir {
+        match entry.kind {
+            Some(RezEntryContentKind::Directory { mut children }) => {
+                create_dir(output_dir.join(entry.path)).expect("being able to create the directory");
+                extract_rez_hierarchy(&mut children, output_dir);
+            }
+            Some(RezEntryContentKind::File { id, content }) => {
+                let mut file = File::create(output_dir.join(entry.path)).expect("being able to create file");
+                file.write(content).expect("being able to write in the file");
+            }
+            None => {}
+        }   
+    }
 }
 
 fn main() {
-    let input = read("GRUNTZ.REZ").expect("file to exist");
+    let mut args = env::args();
+    let input = args.nth(1).expect("input REZ file");
+    let output = args.next().expect("output directory");
+    let input = read(input).expect("file to exist");
+    create_dir(&output).expect("being able to create the root directory");
     let header = RezHeader {
         description: String::from_utf8_lossy(&input[0..127]),
         version: u32::from_le_bytes(input[127..131].try_into().unwrap()),
@@ -97,15 +143,12 @@ fn main() {
         file_name_max: u32::from_le_bytes(input[159..163].try_into().unwrap()) as usize,
     };
     println!("header: {header:#?}");
-    parse_entry(&input, header.dir_offset, header.dir_offset + header.dir_size, Path::new(""), |entry: RezEntry| {
-        match entry.content.kind {
-            Some(RezEntryContentKind::Directory) => {
-                println!(">> dir: {}", entry.content.path.to_str().expect("path to be valid string"))
-            }
-            Some(RezEntryContentKind::File { id, content }) => {
-                println!("- file: {}", entry.content.path.to_str().expect("path to be valid string"));
-            }
-            None => {}
-        }
-    })
+    let mut root_iterator = RezDirectoryIterator {
+        input: &input,
+        path: Path::new("").to_path_buf(),
+        offset: header.dir_offset,
+        end: header.dir_offset + header.dir_size,
+    };
+    display_rez_hierarchy(&mut root_iterator);
+    extract_rez_hierarchy(&mut root_iterator, Path::new(&output));
 }
